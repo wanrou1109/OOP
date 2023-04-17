629c65af9f819415ee28869aba163966b5964079
rou <118650467+wanrou1109@users.noreply.github.com>
2023-04-17 13:51:02 +0800

    Create Lab17.java

---
+++ Lab17/Lab17.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+public class Lab17 {
+    public static void main(String[] args) {
+
+        // TODO Auto-generated method stub
+        JFrame frame = new CalcFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+    }
+}
